94cd3f8a5fca67630aabae9e856479f07bfa1884
Dumukenova Aitunuk <dumukenova05@gmail.com>
2022-04-06 14:45:26 +0600

    corect a proble

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import Nav from "./components/Nav/Nav";
-
 function App() {
   return (
     <div className="App">
-    <Nav />
-    <Header />
-    <Footer />
+      dgfdgdgdd
     </div>
   );
 }
