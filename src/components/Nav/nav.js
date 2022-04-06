6e49250a742aaacadac20ccb5a8669f56b1f9ce4
Dumukenova Aitunuk <dumukenova05@gmail.com>
2022-04-06 14:30:18 +0600

    tr to fix

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -2,10 +2,16 @@ function Nav() {
     return (
       <div className="Nav">
      <ul>
-         <li></li>
-         <li></li>
-         <li></li>
-     </ul>
+         <li>Home</li>
+         <li>New</li>
+         </ul>
+         <h1>books</h1>
+         <ul>
+        <li>For Teenagers</li>
+         <li>Child</li>
+         <li>Shool books</li>
+         </ul>
+     
       </div>
     );
   }
