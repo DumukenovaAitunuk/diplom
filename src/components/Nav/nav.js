84e655b1ef432b34c5d675662dd423d983ae5000
Dumukenova Aitunuk <dumukenova05@gmail.com>
2022-04-06 14:06:09 +0600

    added elements

---
+++ src/components/Nav/nav.js
@@ -0,0 +1,14 @@
+function Nav() {
+    return (
+      <div className="Nav">
+     <ul>
+         <li></li>
+         <li></li>
+         <li></li>
+     </ul>
+      </div>
+    );
+  }
+  
+  export default Nav;
+  
