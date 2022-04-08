48f415caee310d8c7837a638eae7eb63aec49732
Dumukenova Aitunuk <dumukenova05@gmail.com>
2022-04-08 15:31:25 +0600

    added elements

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Navigation from "./Components/Navigation/Navigation";
+
+function App() {
+  return (
+    <div className="App">
+    <Navigation/>
+    <Header/>
+    <Footer/>
+    </div>
+  );
+}
+
+export default App;
